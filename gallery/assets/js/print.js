d3099d444e303124dcf8bab78313d83943bfe9af
Austin Tebbs <stinden1@yahoo.com>
2019-10-22 10:49:52 -0600

    added more models to the gallery, as well as a map to see local printers

--- gallery/assets/js/print.js
+++ gallery/assets/js/print.js
@@ -6,7 +6,7 @@ function test(e) {
     console.log(e);
     console.log(test);
 
-    var title = test.getElementsByTagName("h5")[0].innerHTML;
+    var title = test.getElementsByTagName("h6")[0].innerHTML;
     var filamentColor = test.getElementsByClassName("card-category")[0].innerHTML;
     var picSrc = test.getElementsByTagName("img")[0].src;
     var price = test.getElementsByClassName("stats")[0].innerHTML;
@@ -36,4 +36,32 @@ function test1(){
 
 function clearStorage(){
     localStorage.clear();
+}
+
+function findPrinter(){
+    console.log("findPrinter");
+    location.href = "./map.html";
+    console.log(location.href);
+}
+
+var pics = [
+    "../assets/img/printers/owner1.jpg",
+    "../assets/img/printers/owner2.jpg",
+    "../assets/img/printers/owner3.jpg",
+    "../assets/img/printers/owner4.jpg",
+    "../assets/img/printers/owner5.jpg",
+    "../assets/img/printers/owner6.jpg",
+    "../assets/img/printers/owner7.jpg",
+    "../assets/img/printers/owner8.jpg",
+    "../assets/img/printers/owner9.jpg",
+    "../assets/img/printers/owner10.jpg",
+]
+
+function fillUsers(){
+
+    var index = Math.floor(Math.random() * 10) + 1;
+
+    var img = document.getElementsByClassName("avatar")[0];
+    img.src = pics[index];
+
 }
